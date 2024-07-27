606480952cae83630517d1c5418ce3e0396e6f7a
tombyrn <thomasbyrnes02@gmail.com>
2024-07-27 14:27:34 -0400

    replaced hero picture

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,18 +5,18 @@ import Link from "next/link";
 export default function Hero() {
     return (
         <section className="m-0 scale-y-110 -translate-y-2 ">
-              <Paper className="p-4 w-full flex justify-center items-center text-center rounded-t-3xl z-40 ">
-                <Grid container spacing={2} direction="row" justifyContent="center">
-                      <Grid item>
-                        <Image src="/cakes/cakehappy.png" alt="Cake Happens" width={600} height={200} />
+              <Paper className="p-8 w-full flex justify-center items-center text-center rounded-t-3xl z-40 border">
+                <Grid container spacing={0} direction="row" justifyContent="center" alignContent="center" className="py-8 bg-white rounded-3xl">
+                      <Grid item className="p-8">
+                        <Image src="/platter.jpg" alt="Cake Happens" width={400} height={200} className="border-2 rounded-xl border-pink-300"/>
                       </Grid>
-                      <Grid item container spacing={5} direction="column" className="max-w-[500px]">
-                        <Grid item  className="font-bold text-pink-300 text-6xl text-right">ABOUT CAKED UP CAFE</Grid>
-                        <Grid item  className="font-bold text-pink-200 text-3xl mb-3 text-right">OUR STORY</Grid>
-                        <Grid item  className="text-left">
-                            Since opening its doors, Caked Up has quickly cultivated a following among locals, celebrities and professional athletes with its wildly flavored cupcakes and custom designed celebration cakes. See what everyone’s buzzing about.
+                      <Grid item container spacing={5} direction="column" justifyContent="center" className="max-w-[500px]">
+                        <Grid item className="font-bold text-pink-300 text-6xl text-right">ABOUT CAKED UP CAFE</Grid>
+                        <Grid item className="font-bold text-pink-200 text-3xl mb-3 text-right">OUR STORY</Grid>
+                        <Grid item className="text-left">
+                            Since opening its doors over 10 years ago, Caked Up has cultivated a following among locals, celebrities and professional athletes with its wildly flavored cupcakes and custom designed celebration cakes. See what everyone’s buzzing about.
                         </Grid>
-                        <Grid item >  <Link href="/about" className="cool-button cool-button--red"> Read More </Link></Grid>
+                        <Grid item >  <Link href="/about" className="cool-button cool-button--pink"> Read More </Link></Grid>
                       </Grid>
                 </Grid>
               </Paper>
