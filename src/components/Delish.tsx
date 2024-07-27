f17034dd0debe688f7083e7bf21347ee413859a6
tombyrn <thomasbyrnes02@gmail.com>
2024-07-27 14:30:42 -0400

    carousel css changes and global css cleanup

--- src/components/Delish.tsx
+++ src/components/Delish.tsx
@@ -6,7 +6,7 @@ export default function Delish() {
         <section className="m-0 p-0 z-50">
               <div className="py-8 italic">
                 <div className="my-4 px-8 text-xl">Check out our features on </div>
-                <Link href={"https://www.delish.com/search/?q=caked%20up%20cafe"}><div className="w-full h-12 scrolling-bg" /></Link>
+                <Link href={"https://www.delish.com/search/?q=caked%20up%20cafe"}><div className="w-full h-12 scrolling-delish" /></Link>
               </div>
 
         </section>
--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -13,7 +13,7 @@ const carouselItems = [
       alt: 'Slide 2',
       component: <div className="text-5xl text-pink-300 font-bold -translate-x-24 lg:-translate-x-96">
         <div className="text-left text-lg text-gray-500 italic">Unique Sweets</div>
-        <Link href="https://cakedup-cafe.square.site/?location=11eb042e23766710b101ac1f6bbbcc9c&menu=&item=407#2"><div className="italic hover:border-b-2"> Order Online </div></Link>
+        <Link href="https://cakedup-cafe.square.site/?location=11eb042e23766710b101ac1f6bbbcc9c&menu=&item=407#2"><div className="italic stroke-outline2"> Order Online </div></Link>
       </div>,
     },
     {
@@ -26,14 +26,14 @@ const carouselItems = [
       alt: 'Slide 4',
       component: <div className="text-5xl text-pink-300 font-bold translate-x-24 lg:translate-x-48 -translate-y-14">
         <div className="text-left text-lg text-gray-100 italic">Cookie and Cake Jars</div>
-        <Link href="https://cakedup-cafe.square.site/?location=11eb042e23766710b101ac1f6bbbcc9c&menu=&item=593#4"><div className="hover:border-b-2"> ORDER NOW </div></Link>
+        <Link href="https://cakedup-cafe.square.site/?location=11eb042e23766710b101ac1f6bbbcc9c&menu=&item=593#4"><div className="stroke-outline"> ORDER NOW </div></Link>
       </div>,
     },
   ];
 
 export default function HomeCarousel() {
       return (
-        <section className="mt-24 flex justify-center">
+        <section className="mt-24 h-[30rem] flex justify-center">
           <Carousel
               autoPlay
               swipe
@@ -46,7 +46,7 @@ export default function HomeCarousel() {
               <div
                 key={index}
                 style={{ backgroundImage: `url(${item.image})` }}
-                className="w-full h-96 bg-cover bg-center text-center items-center flex justify-center bg-transparent"
+                className="w-full h-[30rem] bg-cover bg-center text-center items-center flex justify-center bg-transparent"
               >{item.component}</div>
             ))}
           </Carousel>
